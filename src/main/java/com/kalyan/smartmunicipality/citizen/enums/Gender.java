778efae0eddd876b52df5aad157d4e2bb5aa05f8
kalyan <kalyankatwal2@gmail.com>
2025-07-13 18:55:22 +0545

    Gender Enums

--- src/main/java/com/kalyan/smartmunicipality/citizen/enums/Gender.java
+++ src/main/java/com/kalyan/smartmunicipality/citizen/enums/Gender.java
@@ -4,12 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum Gender {
-    MALE("Male"),
-    FEMALE("Female"),
-    OTHERS("Others");
-    private final String label;
-    Gender(String label) {
-        this.label = label;
-    }
+    MALE,
+    FEMALE,
+    OTHERS
+
 
 }
