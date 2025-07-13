fb1b5f492e0d825824bce87e3deeb921cef2214a
kalyan <kalyankatwal2@gmail.com>
2025-07-13 18:57:17 +0545

    Relation enum

---
+++ src/main/java/com/kalyan/smartmunicipality/certificate/enums/Relation.java
@@ -0,0 +1,15 @@
+package com.kalyan.smartmunicipality.certificate.enums;
+
+public enum Relation {
+    FATHER,
+    MOTHER,
+    SPOUSE,
+    SON,
+    DAUGHTER,
+    BROTHER,
+    SISTER,
+    UNCLE,
+    AUNT,
+    GRANDPARENT,
+    OTHER
+}
